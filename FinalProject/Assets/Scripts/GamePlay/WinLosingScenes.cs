ae89955aa33b7f79069e8eab8380cfc004499990
jack6412 <jack6chang@gmail.com>
2021-02-20 21:56:43 -0500

    Reupload the change
    -Main menu and login title.
    -hold the error of game play mode.
    -reorganize the script

---
+++ FinalProject/Assets/Scripts/GamePlay/WinLosingScenes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class WinLosingScenes : MonoBehaviour
+{
+    public GameObject ButtonAction;
+    public GameObject WinUI;
+
+    public void ButtonRestart()
+    {
+        WinUI.gameObject.SetActive(false);
+        ButtonAction.gameObject.SetActive(true);
+        SceneManager.LoadScene("PickNumber");
+    }
+
+    public void ButtonQuit()
+    {
+        WinUI.gameObject.SetActive(false);
+        ButtonAction.gameObject.SetActive(true);
+        SceneManager.LoadScene("Login");
+    }
+}
---
+++ FinalProject/Assets/Scripts/LogIn/LoadScenes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+    public void PlayerVsComputer()
+    {
+        SceneManager.LoadScene("PickNumber");
+    }
+    
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
